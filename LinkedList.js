cfb8e322a0695c1971cb4b74d44adec1d0490046
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-11-21 03:24:58 +0530

    Create LinkedList.js

---
+++ LinkedList.js
@@ -0,0 +1,6 @@
+class Node {
+	constructor(value) {
+		this.value = value; // The value property can hold anything - String, number etc
+		this.next = null; // The next propery is initially set to null
+	}
+}
